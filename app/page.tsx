1bb4d643948f823391907d02c24b8cd63296d41e
Yuvraj Singh <yuvviirajput@gmail.com>
2024-05-28 14:53:18 +0530

    Building Twitter Side Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import {BsBell, BsBookmark, BsEnvelope, BsTwitter} from 'react-icons/bs'
-import { Inter } from "next/font/google";
-import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
+import {Inter, Quicksand} from "next/font/google";
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
+import FeedCard from "@/components/FeedCard";
+import { SlOptions } from "react-icons/sl";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"]})
+const quicksand = Quicksand({ subsets: ["latin"]});
 
 interface TwitterSidebarButton{
   title: string;
@@ -33,32 +36,53 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
     icon: <BsBookmark/>
   },
   {
+    title: 'Twitter Blue',
+    icon: <BiMoney/>
+  },
+  {
     title: 'Profile',
     icon: <BiUser/>
+  },
+  {
+    title: 'More Option',
+    icon: <SlOptions/>
   }
+  
 
 ]
 
 export default function Home() {
   return (
-    <div className="inter.className">
+    <div className={quicksand.className}>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3 pt-8 px-4">
-          <div className="text-4xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
+        <div className="col-span-3 pt-1 ml-19">
+          <div className="text-2xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
             <BsTwitter/>
           </div>
-          <div className="mt-4 text-2xl pr-4">
+          <div className="mt-1 text-2xl pr-4">
             <ul>
-              {sidebarMenuItems.map(item => <li className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-5 py-2 w-fit cursor-pointer mt-2" key={item.title}><span>{item.icon}</span><span>{item.title}</span></li>)}
+              {sidebarMenuItems.map(item => <li className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-4 py-3 w-fit cursor-pointer mt-2" key={item.title}><span className="text-3xl">{item.icon}</span><span>{item.title}</span></li>)}
             </ul>
             <div className="mt-5 px-3">
-              <button className="bg-[#1d9bf0] p-4 text-lg rounded-full w-full font-semibold">Tweet</button>
+              <button className="bg-[#1d9bf0] p-4 text-lg py-2 px-4 rounded-full w-full font-semibold">Tweet</button>
             </div>
             
           </div>
         
         </div>
-        <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-400"></div>
+        <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-500 ">
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          <FeedCard/>
+          
+        </div>
         <div className="col-span-3"></div>
       </div>
 
